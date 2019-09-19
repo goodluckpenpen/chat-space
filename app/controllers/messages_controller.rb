25b725e95cfcf6493874c60037bce501a56a4664
goodluckpenpen <neipia_number@icloud.com>
2019-09-19 16:00:25 +0900

    carrierwave

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,12 @@ class MessagesController < ApplicationController
   def index
   end
 
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path, notice: 'メッセージを投稿しました'
+    else
+      render :new
+    end
+  end
 end
