7c799ed11aac1be6d4b1e23610ade36b104bc000
goodluckpenpen <neipia_number@icloud.com>
2019-09-24 11:11:08 +0900

    hidouki

--- app/assets/JavaScripts/message.js
+++ app/assets/JavaScripts/message.js
@@ -1,8 +1,32 @@
 $(function(){
+  function buildHTML(message){
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data_id="${message.id}">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      <div>
+                      ${content}
+                      </div>
+                      ${img}
+                    </p>
+                  </div>
+                </div>`
+      return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).arrt('action')
+    var url = $(this).attr('action');
+
     $.ajax({
       url: url,
       type: "POST",
@@ -10,6 +34,18 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $("#new_message")[0].reset();
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    })
+    .always(function(){
+      $('.form__submit').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error')
     })
   })
 })
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
-json.text  @message.text
-json.user_id  @message.user_id
-json.user_name  @message.user_name
-json.group_id  @message.group_id
-json.group_name  @message.group_name
+json.id      @message.id
+json.content @message.content 
+json.date    @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.user_name @message.user.name
+json.image   @message.image.url
