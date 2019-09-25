8c884925094820a10c147462997768ef7d441111
goodluckpenpen <neipia_number@icloud.com>
2019-09-25 11:53:22 +0900

    incrimental

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      # binding.pry
+      format.html
+      format.json
+    end
+  end
 
   def edit
   end
@@ -9,6 +18,11 @@ class UsersController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def search
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id:current_user)
+    render json: @users
   end
 
   private
