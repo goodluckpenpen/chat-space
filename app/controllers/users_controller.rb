dcb185228fd64f56e3b624de5490d1ad9c5f5db4
goodluckpenpen <neipia_number@icloud.com>
2019-09-25 13:10:02 +0900

    syusei1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,11 +19,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def search
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
-    render json: @users
-  end
-
   private
 
   def user_params
