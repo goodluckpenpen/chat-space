af08d52a055dc86cb10a3825b70624e973d17751
goodluckpenpen <neipia_number@icloud.com>
2019-09-26 17:40:04 +0900

    autokkinou

--- app/assets/JavaScripts/message.js
+++ app/assets/JavaScripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     var content = message.content ? `${ message.content }` : "";
     var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="message" data_id="${message.id}">
+    var html = `<div class="message" data-id="${message.id}">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
                       ${message.user_name}
@@ -48,4 +48,28 @@ $(function(){
       alert('error')
     })
   })
-})
+
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("id");
+    $.ajax({
+      url: 'api/messages',
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      messages.forEach(function (message){
+        insertHTML = buildHTML(message);
+      $('.messages').append(insertHTML);
+      })
+     $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    })
+    .fail(function() {
+      alert('error')
+    });
+   }
+  };
+    setInterval(reloadMessages, 5000);
+ });
